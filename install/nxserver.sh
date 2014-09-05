52a031563d733c7a7de9ef7a01b3ace53925c291
Matt Fornaciari <mattforni@gmail.com>
2014-09-05 09:19:40 -0700

    Added missing sudo for nxserver install script

--- install/nxserver.sh
+++ install/nxserver.sh
@@ -11,7 +11,7 @@ BIN_LINK_PATH="/usr/bin/nxserver"
 
 echo "Installing NX Server."
 sudo apt-get install nxserver
-ln -s $INSTALL_PATH $BIN_LINK_PATH
+sudo ln -s $INSTALL_PATH $BIN_LINK_PATH
 
 echo "NX Server has been installed."
 
