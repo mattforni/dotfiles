b99a419d923728ab8f0a1503e155431218bb3780
Matt Fornaciari <mattforni@gmail.com>
2014-09-10 13:53:39 -0700

    Updated install script to properly remove all unnecessary files

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
 HOME=$(readlink -f ~)
+EXCLUDED=($(basename $0) install README.md)
 echo -n "Install productivity tools to '$HOME'? [Y/n] "
 read INSTALL
 if [[ "$INSTALL" == "y" || "$INSTALL" == "Y" ]]; then
     CURR_DIRECTORY=$(dirname `readlink -f $0`)
     cp -r $CURR_DIRECTORY/* $HOME
-    rm -f $HOME/$(basename $0)
+    for ITEM in ${EXCLUDED[@]}; do
+        rm -rf $HOME/$ITEM
+    done
     source $HOME/.bashrc
     echo "Productivity tools installed!"
 else
