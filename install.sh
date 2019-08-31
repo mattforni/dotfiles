ee5f772f7fda98a8c57740770ddf3910b4bfe93b
Matthew Fornaciari <mattforni@gmail.com>
2019-08-31 07:29:03 -0700

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-## TODO this needs a lot of work to be usable
-
 EXCLUDED=($(basename $0) README.md .git .gitignore)
 echo -n "Install productivity tools to '$HOME'? [Y/n] "
 read INSTALL
 if [[ "$INSTALL" == "y" || "$INSTALL" == "Y" ]]; then
-    CURR_DIRECTORY=$(dirname `readlink -f $0`)
-    cp -r $CURR_DIRECTORY/. $HOME
+    DIR=$(pwd)
+    echo "Copying files from $DIR ..."
+    cp -r $DIR/. $HOME
     for ITEM in ${EXCLUDED[@]}; do
         rm -rf $HOME/$ITEM
     done
