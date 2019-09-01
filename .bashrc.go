c24223ac94fe74aca34475b3401ab49d76564def
Matthew Fornaciari <mattforni@gmail.com>
2019-08-31 19:37:12 -0700

    Added in basic go bashrc

---
+++ .bashrc.go
@@ -0,0 +1,5 @@
+export GOPATH=$HOME/go-workspace # don't forget to change your path correctly!
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
