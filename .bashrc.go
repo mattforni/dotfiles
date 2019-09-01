b746b7d8e0fbb61627a1001f1b30c6be9f3fed2c
Matthew Fornaciari <mattforni@gmail.com>
2019-08-31 20:12:28 -0700

    Added GOBIN export to .bashrc.go

--- .bashrc.go
+++ .bashrc.go
@@ -2,6 +2,7 @@ export GOPATH=$HOME/Development/go
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+export GOBIN=$GOPATH/bin
 
 # Create the necessary paths if they do not exist
 mkdir -p $GOPATH $GOPATH/src $GOPATH/pkg $GOPATH/bin
