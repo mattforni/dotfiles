83df58569636344a7d136158a7ba2d14fd9d819a
Matthew Fornaciari <mattforni@gmail.com>
2020-03-25 09:40:47 -0700

    Updated install.sh with more relevant messaging

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 EXCLUDED=($(basename $0) README.md .git .gitignore)
-echo -n "Install productivity tools to '$HOME'? [Y/n] "
+echo -n "Do you want to install dotfiles to '$HOME'? [Y/n] "
 read INSTALL
 if [[ "$INSTALL" == "y" || "$INSTALL" == "Y" ]]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    echo "Copying files from $DIR ..."
+    echo "Installing dotfiles from $DIR ..."
     cp -r $DIR/. $HOME
     for ITEM in ${EXCLUDED[@]}; do
         rm -rf $HOME/$ITEM
     done
     source $HOME/.bashrc
-    echo "Productivity tools installed!"
+    echo "dotfiles installed!"
 else
-    echo "Productivity tools not installed."
+    echo "Unable to install dotfiles."
 fi
 
