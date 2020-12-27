4d78b9521c12dfa3c619420bdcd9bca1571b75b0
Matthew Fornaciari <mattforni@gmail.com>
2020-12-26 19:02:37 -0800

    Added a zsh terminal config file and some basic configurations

---
+++ .zsh/terminal.zsh
@@ -0,0 +1,6 @@
+# Inspired by [my-oh-my-zsh](https://github.com/mattjj/my-oh-my-zsh)
+# Sets terminal window and tab titles.
+
+PS1='%n@%m %# '     # Sets the prompt to <username>@<hostname>
+RPS1='%100<...<%/'  # Sets the right prompt to the current working directo (truncated to 100 characters)
+
