e8c39f7c3e78d45dc08183960289b522fa54a13b
Matthew Fornaciari <mattforni@gmail.com>
2023-10-10 15:45:03 -0600

    Latest and greatest

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -19,3 +19,6 @@ setopt HIST_VERIFY               # Don't execute immediately upon history expans
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 
+bindkey -v
+bindkey '^R' history-incremental-search-backward
+
